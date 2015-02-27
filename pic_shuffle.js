f76b97bb88404642086981d51369d3b08f5a642d
Hans D. Stroo <hans.stroo@gmail.com>
2015-02-27 13:01:05 -0800

    first commit

--- pic_shuffle.js
+++ pic_shuffle.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
       $(this).replaceWith('<li id="bdPic" a href="http://en.wikipedia.org/wiki/Bulldog"> <img src="http://i.imgur.com/S55TFVC.png"> </img> </a> </li> ');
     }, function() {
       $(this).replaceWith('<li id="bdNavItem"> <a href="http://en.wikipedia.org/wiki/Bulldog"> Bulldogs </a> </li>');
-  }
 );
 
 });
