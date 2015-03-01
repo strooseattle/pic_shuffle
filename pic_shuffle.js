8f8c71c22825e950b3f6708654ab707ad2c24d33
Hans D. Stroo <hans.stroo@gmail.com>
2015-03-01 11:46:31 -0800

    add fixed position nav bar. Make sharp edges on pictures. Add hover features on nav links & #launch button.

--- pic_shuffle.js
+++ pic_shuffle.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
 
-  $('#bdNavItem').hover(
-    function() {
-      $(this).replaceWith('<li id="bdPic" a href="http://en.wikipedia.org/wiki/Bulldog"> <img src="http://i.imgur.com/S55TFVC.png"> </img> </a> </li> ');
-    }, function() {
-      $(this).replaceWith('<li id="bdNavItem"> <a href="http://en.wikipedia.org/wiki/Bulldog"> Bulldogs </a> </li>');
-);
+  $( '#bdNavItem' ).hover(function() {
+    $( this ).html('<img src="http://i.imgur.com/S55TFVC.png>" </img>');
+    $( this ).html('<a href="http://en.wikipedia.org/wiki/Bulldog"> Bulldogs </a>');
+  });
 
 });
